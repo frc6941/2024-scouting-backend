06142b0bd94d4486b2b6a1a7722031ec1a926fa5
Evan Luo <knacks.insects_0o@icloud.com>
2024-02-23 11:54:47 +0800

    build: Add default super user

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
 pip install -r requirements.txt
+python3.9 manage.py migrate
 echo yes | python3.9 manage.py collectstatic
+python3.9 manage.py createsuperuser --noinput
--- scouting_backend_2024/settings.py
+++ scouting_backend_2024/settings.py
@@ -129,3 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
