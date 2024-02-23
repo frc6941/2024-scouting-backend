45476f2db60da99f632ff0dac4a9b25858fc3a8d
Evan Luo <knacks.insects_0o@icloud.com>
2024-02-23 11:12:51 +0800

    build: Re-add collectstatic

---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+echo yes | python3.9 manage.py collectstatic
--- scouting_backend_2024/settings.py
+++ scouting_backend_2024/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
