143a1385696328a13909f759191d154f9868445b
Evan Luo <knacks.insects_0o@icloud.com>
2024-02-27 07:58:04 +0800

    feat: Register pit team

--- scouting/admin.py
+++ scouting/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from scouting.models import Record
+from scouting.models import Record, PitTeam
 
 # Register your models here.
 admin.site.register(Record)
+admin.site.register(PitTeam)
